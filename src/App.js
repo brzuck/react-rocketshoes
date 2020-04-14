434f4fb79c09e25a491cd29895ac8941e0b662d4
Bruno Barros <bruno@cubeti.com.br>
2020-04-14 18:23:47 -0300

    creating example app using flux architecture

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
+import './config/reactotronConfig';
+import Routes from './routes';
+import history from './services/history';
+import store from './store';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
